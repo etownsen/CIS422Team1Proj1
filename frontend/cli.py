6ccb01ab876e7f7c3b314b19936785b8c2fb853e
Kevin Beick <kevin beick@Kevin-Beicks-Computer.local>
2014-01-23 18:52:10 -0800

    'options' and 'quit' commands added

--- frontend/cli.py
+++ frontend/cli.py
@@ -5,10 +5,33 @@
 	:author: Kevin Beick
 """
 
+import sys
 from cmd import Cmd
 
 # TODO import dependencies from backend
 
+WELCOME_MESSAGE = \
+	"\nWelcome to Blue Book!\n" + \
+	"Type \"help\" to view available command options.\n"
+
+OPTIONS_MESSAGE = \
+	"VALID KEYWORDS:\n" + \
+	"add\n" + \
+	"edit\n" + \
+	"delete\n" + \
+	"display\n" + \
+	"quit\n" + \
+	"options\n" + \
+	"help\n\n" + \
+	\
+	"VALID FLAGS (used with keywords edit, delete, display):\n" + \
+	"-fn (first name)\n" + \
+	"-ln (last name)\n" + \
+	"-a (address)\n" + \
+	"-c (city)\n" + \
+	"-s (state)\n" + \
+	"-z (ZIP Code)\n" + \
+	"-e (email)\n"
 
 
 class CommandLineInterface(Cmd):
@@ -20,7 +43,7 @@ class CommandLineInterface(Cmd):
 	delete
 	display
 	quit
-	help
+	options
 
 	TODO: 
 	import
@@ -37,14 +60,28 @@ class CommandLineInterface(Cmd):
 
 	"""
 
-	intro = "\nWelcome to Blue Book!\n" + \
-			"Type \"help\" to view available command options.\n"
+	intro = WELCOME_MESSAGE
 
-	options = ("add", "edit", "delete", "display", "quit", "help")
+	options = ("add", "edit", "delete", "display", "quit", "options")
 	flags = ("-fn", "-ln", "-a", "-c", "-s", "-z", "-e")
+
+
+	def do_options(self, line):
+		"""
+		Displays the available keyword commands used by the applet.
+		"""
+		print OPTIONS_MESSAGE
+
+	def do_quit(self, line):
+		"""
+		quit the applet
+		"""
+		print "\n"
+		sys.exit()
 
 
 
 if __name__ == "__main__":
 	command = CommandLineInterface()
 	command.cmdloop()
+
